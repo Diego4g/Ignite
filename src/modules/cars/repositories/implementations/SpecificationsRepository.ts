eea32dbc015d6ac4ac7532397d3463a1c5a6aa72
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-04 01:40:54 -0300

    Aplicando injeçao de dependencia

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,27 +1,29 @@
+import { getRepository, Repository } from "typeorm";
 import { Specification } from "../../entities/Specification";
 import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository"
 
 
 class SpecificationsRepository implements ISpecificationsRepository {
-    private specifications: Specification[];
+    private repository: Repository<Specification>
 
     constructor() {
-        this.specifications = [];
+        this.repository = getRepository(Specification);
     }
-    create({ description, name }: ICreateSpecificationDTO): void {
-        const specification = new Specification();
 
-        Object.assign(specification, {
+    async create({ description, name }: ICreateSpecificationDTO): Promise<void> {
+       const specification = this.repository.create({
+           description,
+           name
+       });
+
+       await this.repository.save(specification)
+    }
+
+    async findByName(name: string): Promise<Specification> {
+        const specification = this.repository.findOne({
             name,
-            description,
-            created_at: new Date(),
         });
 
-        this.specifications.push(specification);
-    }
-
-    findByName(name: string) {
-        const specification = this.specifications.find(specification => specification.name == name);
         return specification;
     }
 
