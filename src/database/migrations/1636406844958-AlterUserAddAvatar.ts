841bcbe478d70915b0393c944ff8e5aa9df36181
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-08 20:18:58 -0300

    feat: upload avatar
    Alem da funcionalidade de acrescentar o avatar, tambem foi adicionado a tratativa de deletar o
    avatar antigo quando um novo e carregado

---
+++ src/database/migrations/1636406844958-AlterUserAddAvatar.ts
@@ -0,0 +1,19 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export class AlterUserAddAvatar1636406844958 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn("users",
+        new TableColumn({
+                name: "avatar",
+                type: "varchar",
+                isNullable: true, // significa que a coluna pode ser nula
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn("users","avatar");
+    }
+
+}
