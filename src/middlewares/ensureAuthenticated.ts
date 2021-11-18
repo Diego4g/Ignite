91c942bfd6d128e0a3da175e5e67185b6e780a66
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-18 02:03:23 -0300

    docs: adding a comment

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -3,16 +3,16 @@ import { verify } from "jsonwebtoken"
 import { AppError } from "../errors/AppError";
 import { UsersRepository } from "../modules/accounts/repositories/implementations/UsersRepository";
 
-interface IPayload{
+interface IPayload {
     sub: string;
 }
 
-export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction){
-    
+export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+
     const authHeader = request.headers.authorization;
 
-    if(!authHeader){
-        throw new AppError("Token missing",401);
+    if (!authHeader) {
+        throw new AppError("Token missing", 401);
     }
 
     //Bearer 8od56a44das45d6ad456a: assim q vem o token
@@ -23,27 +23,27 @@ export async function ensureAuthenticated(request: Request, response: Response, 
     //[token] = 8od56a44das45d6ad456a
 
     // Quando a função verify atesta que o token é invalido, ela lança uma excessao, e por isso utilizamos o try catch aqui
-    try{
+    try {
         const { sub: user_id } = verify(token, "09bc7047827eddf304f5cbf40b5bb859") as IPayload; // A função verify recebe o token e o código secreto que a gente gerou no md5 hash generator
         // note que o verify retorna 3 atributos: 
         //iat: quando o token foi criado
         //exp: quando o token expira
-        //sub: que é o token de fato
+        //sub: que é o id
         //Porem fazendo apenas a desestruturação const { sub } = verify... nao é possivel, entao forçamos isso com a interface IPayload
-        
+
         const usersRepository = new UsersRepository();
 
         const user = usersRepository.findById(user_id);
 
-        if(!user){
-            throw new AppError("User does not exists!",401);
+        if (!user) {
+            throw new AppError("User does not exists!", 401);
         }
 
         request.user = {
             id: user_id
         }
         next();
-    }catch{
-        throw new AppError("Invalid token!",401);
+    } catch {
+        throw new AppError("Invalid token!", 401);
     }
 }
