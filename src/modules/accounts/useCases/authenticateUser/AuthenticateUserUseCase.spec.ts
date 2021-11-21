cb84ae414d0b34af55ef3ecc8e8818bb723fdd95
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-21 00:37:37 -0300

    refactor: changing directories

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,6 +1,8 @@
-import { AppError } from "@errors/AppError"
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
 import { UsersRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersRepositoryInMemory";
+
+import { AppError } from "@shared/errors/AppError"
+
 import { CreateUserUseCase } from "@modules/accounts/useCases/createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase"
 
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,15 +1,17 @@
+import "reflect-metadata"
 import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors"
 import swaggerUi from "swagger-ui-express";
 
-import "./database";
+import "@shared/infra/typeorm";
 
 import "@shared/container";
 
-import { router } from './routes'
-import swaggerFile from "./swagger.json";
-import { AppError } from '@errors/AppError';
+import { AppError } from '@shared/errors/AppError';
 
+import swaggerFile from "../../../swagger.json";
+
+import { router } from './routes'
 
 
 const app = express();
