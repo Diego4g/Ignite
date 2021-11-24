d985e799b95c6c2bfbf107e8a24376ad5b22d8ea
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-23 23:34:52 -0300

    feat: adding functionality for listing cars
    Adicionando funcionalidade que retorna uma lista de veiculos com status "available" = true, essa
    listagem tambem pode ser feita pela marca, nome, ou ate mesmo o id da categoria do carro, desde que
    a propriedade available seja verdadeira.

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -5,9 +5,8 @@ import { ICarsRepository } from "../ICarsRepository";
 
 
 class CarsRepositoryInMemory implements ICarsRepository {
+
     cars: Car[] = [];
-
-
 
     async create({ brand, category_id, daily_rate, description, fine_amount, name, license_plate }: ICreateCarDTO): Promise<Car> {
         const car = new Car()
@@ -30,6 +29,20 @@ class CarsRepositoryInMemory implements ICarsRepository {
     async findByLicensePlate(license_plate: string): Promise<Car> {
         return this.cars.find((car) => car.license_plate === license_plate);
 
+    }
+
+    async findAvailable(brand?: string, category_id?: string, name?: string): Promise<Car[]> {
+        const cars = this.cars
+            .filter((car) => {
+                if (car.available === true ||
+                    ((brand && car.brand === brand) ||
+                        (category_id && car.category_id === category_id) ||
+                        (name && car.name === name))) {
+                    return car;
+                }
+                return null;
+            })
+        return cars;
     }
 
 }
