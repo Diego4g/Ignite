46f7257b0f8c16ceb1e90bfcf350534f809fe296
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-24 02:19:35 -0300

    feat: adding functionality for create specification of cars

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -13,7 +13,7 @@ class CarsRepository implements ICarsRepository {
         this.repository = getRepository(Car);
     }
 
-    async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name }: ICreateCarDTO): Promise<Car> {
+    async create({ brand, category_id, daily_rate, description, fine_amount, license_plate, name, specifications, id }: ICreateCarDTO): Promise<Car> {
         const car = this.repository.create({
             brand,
             category_id,
@@ -21,7 +21,9 @@ class CarsRepository implements ICarsRepository {
             description,
             fine_amount,
             license_plate,
-            name
+            name,
+            specifications,
+            id,
         });
 
         await this.repository.save(car);
@@ -54,6 +56,11 @@ class CarsRepository implements ICarsRepository {
         const cars = await carsQuery.getMany();
 
         return cars
+    }
+
+    async findById(id: string): Promise<Car> {
+        const car = await this.repository.findOne(id);
+        return car
     }
 
 }
