a1b3f4982ea20de1e944c26daa11e440416b251e
Diego Ferreira <diegodejesusferreira2@gmail.com>
2021-11-29 15:30:38 -0300

    Adding test for CreateStatementUseCase

--- src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementUseCase.spec.ts
@@ -1,6 +1,12 @@
+import { AppError } from "../../../../shared/errors/AppError";
 import { InMemoryUsersRepository } from "../../../users/repositories/in-memory/InMemoryUsersRepository";
 import { InMemoryStatementsRepository } from "../../repositories/in-memory/InMemoryStatementsRepository";
 import { CreateStatementUseCase } from "./CreateStatementUseCase"
+
+enum OperationType {
+  DEPOSIT = 'deposit',
+  WITHDRAW = 'withdraw',
+}
 
 let createStatementUseCase: CreateStatementUseCase;
 let inMemoryStatementsRepository: InMemoryStatementsRepository;
@@ -21,17 +27,36 @@ describe("Create Statement", () => {
       name: "test statement",
       password: "test231"
     })
-
-
     const statement = await createStatementUseCase.execute({
       user_id: user.id,
       amount: 100,
       description: "test create statement",
       type: null
     })
-
-    console.log(statement)
-
     expect(statement).toHaveProperty("id")
   })
+
+  it("should not be able create a statement with type withdraw if funds is insufficient", () => {
+
+    expect(async () => {
+      const user = await inMemoryUsersRepository.create({
+        email: "test@test.com",
+        name: "test statement",
+        password: "test231"
+      })
+
+      let type = "withdraw" as OperationType;
+
+      const statement = await createStatementUseCase.execute({
+        user_id: user.id,
+        amount: 1000,
+        description: "test create statement",
+        type: type
+      })
+
+      console.log(statement)
+
+    }).rejects.toBeInstanceOf(AppError)
+  })
+
 })
