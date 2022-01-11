5e5d3cb7068d54bc808672c29029ad36b77b74df
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-01-11 16:47:37 -0300

    Adding functionality for clients authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
+import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/CreateClient/CreateClientController";
 
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
 
-routes.post("/client/", createClientController.handle)
+routes.post("/authenticate", authenticateClientController.handle);
+routes.post("/client/", createClientController.handle);
 
 
 export { routes }
