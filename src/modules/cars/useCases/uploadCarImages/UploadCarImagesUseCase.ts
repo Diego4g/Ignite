58816bade339c8f41c6c124b5635f813175ab0f5
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-01-18 22:29:14 -0300

    refactor: adding functionality for upload image car in S3

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,5 +1,8 @@
+/* eslint-disable prettier/prettier */
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
 import { inject, injectable } from "tsyringe";
+
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
     car_id: string;
@@ -10,12 +13,15 @@ interface IRequest {
 class UploadCarImagesUseCase {
     constructor(
         @inject("CarsImagesRepository")
-        private carsImagesRepository: ICarsImagesRepository
-    ) {}
+        private carsImagesRepository: ICarsImagesRepository,
+        @inject("StorageProvider")
+        private storageProvider: IStorageProvider
+    ) { }
 
     async execute({ car_id, images_name }: IRequest): Promise<void> {
         images_name.map(async (image) => {
             await this.carsImagesRepository.create(car_id, image);
+            await this.storageProvider.save(image, "cars")
         });
     }
 }
