c58b23090e390d0595ec170156aa5bfabf95c5bf
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-02-06 01:29:31 -0300

    added styles

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,22 +1,21 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss'
 const repository = {
-    name: 'unform',
-    description: 'Froms in React',
-    link: 'https://github/unform/unform'
+  name: 'unform',
+  description: 'Froms in React',
+  link: 'https://github/unform/unform'
 }
 
 export function RepositoryList(){
-    return (
-        <section className="repository-list">
-            <h1>Lista de repositórios</h1>
-
-            <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-               
-            </ul>
-        </section>
-    )
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+        <ul>
+          <RepositoryItem repository={repository}/>
+          <RepositoryItem repository={repository}/>
+          <RepositoryItem repository={repository}/>
+        </ul>
+    </section>
+  )
 }
