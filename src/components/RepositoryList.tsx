b2d007ba64c29da0c56e2e9506cb676e624c340c
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-02-06 03:29:30 -0300

    alterando todo codigo para typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,11 +3,18 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'  
 import { useState, useEffect } from "react";
 
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList(){
   // como demora um pouco para a api nos responder, devemos fazer com que
   // nossa app fique pronta pra atualizar os dados quando eles estiverem prontos
   // para isso utilizamos o useState
-  const [repositories, setRepositores] = useState([]); 
+  const [repositories, setRepositores] = useState<Repository[]>([]); 
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
