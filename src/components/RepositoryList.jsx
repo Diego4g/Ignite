3c6c0cc05ce2a6069348d273c01a116d8efca65d
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-02-06 02:21:54 -0300

    consumindo github/repos

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,20 +1,27 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-import '../styles/repositories.scss'
-const repository = {
-  name: 'unform',
-  description: 'Froms in React',
-  link: 'https://github/unform/unform'
-}
+import '../styles/repositories.scss'  
+import { useState, useEffect } from "react";
 
 export function RepositoryList(){
+  // como demora um pouco para a api nos responder, devemos fazer com que
+  // nossa app fique pronta pra atualizar os dados quando eles estiverem prontos
+  // para isso utilizamos o useState
+  const [repositories, setRepositores] = useState([]); 
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(data => setRepositores(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
         <ul>
-          <RepositoryItem repository={repository}/>
-          <RepositoryItem repository={repository}/>
-          <RepositoryItem repository={repository}/>
+          {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository}/>
+          })}
         </ul>
     </section>
   )
