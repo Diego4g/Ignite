d9442ea61afe08fec2b322824c68fd8133616b71
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-02-10 02:06:35 -0300

    Challenge finish

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== ""){
+      const newTask:Task = {
+        id: Math.random()*100,
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks([...tasks, newTask]);
+      setNewTaskTitle("")
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const updateTaskById = tasks.map((task) => task.id === id ? 
+      {
+        ...task,
+        isComplete: !task.isComplete        
+      }: task)
+      setTasks(updateTaskById);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const RemoveTaskById = tasks.filter((task) => task.id !== id )
+
+    setTasks(RemoveTaskById)
   }
 
   return (
