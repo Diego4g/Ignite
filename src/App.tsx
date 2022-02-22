d4a1f41ce493d71f1faa86554fcbd1bd3aa09c79
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-02-21 21:22:20 -0300

    refactor: styles modified in create transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
+Modal.setAppElement('#root');
+
 export function App() {
+
+  const [isNewTransactionsModalOpen, setIsNewTransactionsModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionsModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionsModalOpen(false);
+  }
+  
   return (
     <>
-     <Header />
-     <Dashboard />
-     <GlobalStyle />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
+      <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionsModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
+      <GlobalStyle />
     </>
   );
 }
